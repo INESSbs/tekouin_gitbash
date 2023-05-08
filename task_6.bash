1ba30b1160fa1cbc796c8211cbc53cca66b929e3
INESSbs <ines.benslimane@esprit.tn>
2023-05-08 11:53:30 +0200

    diplay 3rd line

--- task_6.bash
+++ task_6.bash
@@ -1,2 +1,1 @@
-head -3 /etc/passwd | tail +3 etc/passwd 
-[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C -1
+head -3 /etc/passwd |tail -1
